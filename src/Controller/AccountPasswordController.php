78ef635c817c96b222341cf592201dbf933c3821
abir Jmail <jmailabir@gmail.com>
2024-01-28 00:54:59 +0100

    chage passe page

---
+++ src/Controller/AccountPasswordController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangepasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\DependencyInjection\Loader\Configurator\request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+// use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AccountPasswordController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    #[Route('/compte/modifier-mon-mot-de-passe', name: 'app_account_password')]
+    public function index(Request $request ,UserPasswordHasherInterface $encoder): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangepasswordType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('old_password')->getData();
+            $user = $form->getData();
+
+            if ($encoder->isPasswordValid($user, $old_pwd)) {
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+
+
+                $this->entityManager->flush();
+                $notification = "Votre mot de passe a bien été mis à jour.";
+            } else {
+                $notification = "Votre mot de passe actuel n'est pas le bon";
+            }
+        }
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView(),
+        //     'notification' => $notification
+         ]);
+    }
+}
