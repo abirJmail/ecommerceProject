cb6d2a92221a001339ffc271a0be5f9c63175aaa
abir Jmail <jmailabir@gmail.com>
2024-01-29 23:37:41 +0100

    nos page et page de produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/nos-produits', name: 'app_product')]
     public function index(): Response
     {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        // dd($products);
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/produit/{slug}', name: 'product')]
+    public function show($slug)
+    {
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        // $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
+
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+            // 'products' => $products
         ]);
     }
 }
+
+
