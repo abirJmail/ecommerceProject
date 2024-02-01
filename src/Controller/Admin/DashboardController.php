ab0083926cff4fb22cd48308acc5157dd6fd26de
abir Jmail <jmailabir@gmail.com>
2024-02-02 00:37:40 +0100

    carrier entity and admin crud

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Carrier;
 use App\Entity\User;
 use App\Entity\Product;
 use App\Entity\Category;
@@ -37,7 +38,7 @@ class DashboardController extends AbstractDashboardController
         // yield MenuItem::linkToCrud('Orders', 'fa fa-shopping-cart', Order::class);
         yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
         yield MenuItem::linkToCrud('Products', 'fa fa-tag', Product::class);
-        // yield MenuItem::linkToCrud('Carriers', 'fa fa-truck', Carrier::class);
+        yield MenuItem::linkToCrud('Carriers', 'fa fa-truck', Carrier::class);
         // yield MenuItem::linkToCrud('Headers', 'fa fa-desktop', Header::class);
     }
 }
