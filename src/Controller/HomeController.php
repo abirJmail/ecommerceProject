d5c66f75bd71ca26e9b982e8a62d8a801c62ad8f
abir Jmail <jmailabir@gmail.com>
2024-02-13 01:01:27 +0100

    header home entity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Header;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,11 +21,11 @@ class HomeController extends AbstractController
     public function index()
     {
         $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
-        // $headers = $this->entityManager->getRepository(Header::class)->findAll();
+        $headers = $this->entityManager->getRepository(Header::class)->findAll();
 
         return $this->render('home/index.html.twig', [
             'products' => $products,
-            // 'headers' => $headers
+            'headers' => $headers
         ]);
     }
 }
